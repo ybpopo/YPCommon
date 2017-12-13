59be4fe00c2e11460ffee3177bb5112e277f6664
Yu <ybpopo@live.com>
2017-12-13 21:27:06 +0800

    submit

---
+++ Helper/DelegateHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YPCommon.Helper
+{
+    /// <summary>
+    /// 委托帮助类
+    /// </summary>
+    public class DelegateHelper
+    {
+        /// <summary>
+        /// 泛型委托
+        /// </summary>
+        /// <typeparam name="T">传入值类型</typeparam>
+        /// <typeparam name="V">返回值类型</typeparam>
+        /// <param name="value">传入值对象</param>
+        /// <returns>返回值对象</returns>
+        public delegate V AsyncDel<T, V>(T value);
+    }
+}
