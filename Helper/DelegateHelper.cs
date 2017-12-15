8599e6c20fe4fab2ac112990f19196bfa79e6996
子鱼 <ybpopo@live.com>
2017-12-15 13:31:41 +0800

    submit

--- Helper/DelegateHelper.cs
+++ Helper/DelegateHelper.cs
@@ -17,13 +17,32 @@ namespace YPCommon.Helper
         /// <typeparam name="V">返回值类型</typeparam>
         /// <param name="value">传入值对象</param>
         /// <returns>返回值对象</returns>
-        public delegate V AsyncDel<T, V>(T value);
+        public delegate V GenericDel<T, V>(T value);
 
         /// <summary>
         /// 泛型委托
         /// </summary>
         /// <typeparam name="T">传入值类型</typeparam>
         /// <param name="value">传入值对象</param>
-        public delegate void AsyncDel<T>(T value);
+        public delegate void GenericDel<T>(T value);
+
+        /// <summary>
+        /// 委托
+        /// </summary>
+        public delegate void Generic();
+
+        /// <summary>
+        /// 异步委托
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static KeyValuePair<IAsyncResult, Generic> SyncDel(Action action)
+        {
+            var del = new Generic(action);
+            var async = del.BeginInvoke(null, null);
+            return new KeyValuePair<IAsyncResult, Generic>(async, del);
+        }
+
+
     }
 }
