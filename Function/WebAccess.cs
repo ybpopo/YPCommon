4abe5e361cf5e4f9681b9fb38e462c85175c22c0
子鱼 <ybpopo@live.com>
2018-03-01 13:55:36 +0800

    submit

--- Function/WebAccess.cs
+++ Function/WebAccess.cs
@@ -108,5 +108,105 @@ namespace YPCommon.Function
             }
         }
 
+
+        /// <summary>
+        /// 将cookie添加到CookieContainer中
+        /// </summary>
+        /// <param name="cookies">cookie字符串</param>
+        /// <param name="domain">cookie的域</param>
+        /// <param name="cookieContainer">CookieContainer</param>
+        public static void AddCookies(string cookies, string domain, ref CookieContainer cookieContainer)
+        {
+            if (string.IsNullOrEmpty(cookies))
+                return;
+
+            if (cookieContainer == null)
+                cookieContainer = new CookieContainer();
+
+            var cookieDict = GetCookieDictionary(cookies);
+            var cookieUri = GetUri(domain);
+
+            var cookieCollection = cookieContainer.GetCookies(cookieUri);
+            if (cookieDict.Count > 0)
+            {
+                if (cookieDict.Count > 20 && cookieDict.Count < 50) cookieContainer.PerDomainCapacity = cookieDict.Count;
+                foreach (var key in cookieDict.Keys)
+                {
+                    var isExist = false;
+                    foreach (Cookie cookie in cookieCollection)
+                    {
+                        if (cookie.Name == key)
+                        {
+                            isExist = true;
+                            cookie.Domain = domain;
+                            cookie.Value = cookieDict[key];
+                            break;
+                        }
+                    }
+
+                    if (!isExist)
+                    {
+                        try
+                        {
+                            var ck = new Cookie(key, cookieDict[key]) { Domain = domain };
+                            cookieContainer.Add(ck);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取cookie信息字典
+        /// </summary>
+        /// <param name="cookies"></param>
+        /// <returns></returns>
+        private static Dictionary<string, string> GetCookieDictionary(string cookies)
+        {
+            var cookieDict = new Dictionary<string, string>();
+            var cookstr = cookies.Split(';');
+            foreach (var str in cookstr)
+            {
+                if (str.Contains("="))
+                {
+                    var index = str.IndexOf("=", StringComparison.Ordinal);
+                    var cname = str.Substring(0, index).Trim();
+                    var value = str.Substring(index + 1, str.Length - index - 1).Trim();
+                    if (!string.IsNullOrEmpty(cname) && !cname.StartsWith("$"))
+                    {
+                        if (!cookieDict.ContainsKey(cname))
+                        {
+                            cookieDict.Add(cname, value);
+                        }
+                        else
+                        {
+                            if (string.IsNullOrEmpty(cookieDict[cname]) && !string.IsNullOrEmpty(value))
+                            {
+                                cookieDict[cname] = value;
+                            }
+                        }
+                    }
+                }
+            }
+            return cookieDict;
+        }
+
+        /// <summary>
+        /// 获取匹配的域名uri
+        /// </summary>
+        /// <param name="domain"></param>
+        /// <returns></returns>
+        private static Uri GetUri(string domain)
+        {
+            Uri cookieUri = domain.StartsWith("http://") ?
+                new Uri(domain) : domain.StartsWith(".") ?
+                new Uri(string.Format("http://www{0}", domain)) :
+                new Uri(string.Format("http://{0}", domain));
+            return cookieUri;
+        }
     }
 }
